aead022a19f0fc813c6195eb624ac66b10d908f7
msaquico <48308936+msaquico@users.noreply.github.com>
2019-03-07 15:06:43 +0100

    Update Footer.vue

--- app/web/src/components/Footer.vue
+++ app/web/src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        MARTINSD Blackbelt Team
+        MARTINSD v2 Blackbelt Team
       </div>
       <div class="col-lg-6">
       </div>
